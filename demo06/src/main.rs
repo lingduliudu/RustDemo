e3b871532aac89ae56efb1a17f7782540e9e1576
Hao.Yuan <lingduliudu@163.com>
2025-11-27 22:05:50 +0800

    Update main.rs

--- demo06/src/main.rs
+++ demo06/src/main.rs
@@ -1,24 +1,8 @@
-use futures::executor::block_on;
-use futures::join;
-use tokio::time::{Duration, sleep};
-#[tokio::main]
-async fn main() {
-    let x = test();
-    block_on(x);
-}
-
-async fn test() {
-    let x = download_01();
-    let y = download_02();
-    join!(x, y);
-}
-
-async fn download_01() {
-    sleep(Duration::from_secs(4)).await;
-    println!("01 完毕!");
-}
-
-async fn download_02() {
-    sleep(Duration::from_secs(7)).await;
-    println!("02 完毕!");
+mod test_box;
+mod test_fs;
+mod test_hashmap;
+fn main() {
+    //test_hashmap::test01();
+    //test_box::test04();
+    test_fs::test02();
 }
