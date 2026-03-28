9cf5993b07857c30b7fe6cc479ba862e754e5fba
lingduliudu <lingduliudu@163.com>
2026-03-28 15:54:44 +0800

    C++ demo

--- demo18/main.c
+++ demo18/main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#ifdef _WIN32
 #include <windows.h>
-#endif
+void test_recursion(int a){
+    if(a == 1){
+        printf("current is %d\n", a);
+
+    } else {
+        printf("current is %d\n", a);
+        test_recursion(a - 1);
+    }
+}
+
 int main(void) {
-    SetConsoleOutputCP(CP_UTF8);
-    #ifdef _MSC_VER
-        printf("当前使用的是 MSVC 编译器。\n");
-        printf("MSVC 版本号: %d\n", _MSC_VER);
-    #elif defined(__clang__)
-        printf("当前使用的是 Clang 编译器。\n");
-    #elif defined(__GNUC__)
-        printf("当前使用的是 GCC 编译器。\n");
-    #else
-        printf("未知编译器。\n");
-    #endif
-    return 0;
+    int a = 12;
+    test_recursion(a);
 }
